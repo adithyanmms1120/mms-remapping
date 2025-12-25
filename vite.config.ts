e3356fd18697acabc13251a0519d13c5ea1247d0
ADITHYAN003 <adithyanadithyanmc@gmail.com>
2025-12-25 15:34:47 +0530

    demo-remapping

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/mms-remapping/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
